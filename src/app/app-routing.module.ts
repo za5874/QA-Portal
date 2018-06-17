03aba54fa6221a8d989d1ce4cbe1a63fd2ebbcbd
za5874 <za5874@gmail.com>
2018-06-17 22:54:12 +0300

    removing ':' in the app routing children

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,8 +24,8 @@ const appRoutes: Routes = [
         { path: ':training-report', component: TrainingReportComponent},
     ] },
     { path: 'qa-engineer', component: QaEngineerComponent, children: [
-        { path: ':status-report', component: StatusReportComponent},
-        { path: ':certiication', component: CertificationComponent},
+        { path: 'certiication', component: CertificationComponent},
+        { path: 'status-report', component: StatusReportComponent},
     ] },
 
     { path: 'qa-manager', component: QaManagerComponent },
